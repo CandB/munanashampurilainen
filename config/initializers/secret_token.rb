c44005ab4e00e457dbfc255035b78a50fdac5ac9
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2014-03-28 20:30:50 +0200

    ror init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Munanashampurilainen::Application.config.secret_key_base = 'b607009504a5222d6bf751ddbfab6793e4ed1fdafac9c116329e39296175e5c20b524af795f08bdbca5cb36cfb0c25097b66da9719579e91cd1c8e4162352630'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Munanashampurilainen::Application.config.session_store :cookie_store, key: '_munanashampurilainen_session'
