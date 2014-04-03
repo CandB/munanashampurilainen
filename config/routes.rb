2af0e0d40a364269beefa70178eae1131019d61a
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2014-04-04 01:47:47 +0300

    Routes mod

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Munanashampurilainen::Application.routes.draw do
   resources :publishers
-
   resources :authors
-
   resources :in_proceedings
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
