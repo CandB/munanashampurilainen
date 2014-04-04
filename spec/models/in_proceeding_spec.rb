0ff70fbce6bfabf83ee15ab1af1b5f1b07de0d56
Julppu <juha.lindqvist@cs.helsinki.fi>
2014-04-04 12:02:46 +0300

    testiä

--- spec/models/in_proceeding_spec.rb
+++ spec/models/in_proceeding_spec.rb
@@ -7,4 +7,12 @@ describe InProceeding do
 	ip.booktitle.should == "Kirjaotsikko"
 	ip.year.should == 2014
   end
+
+  it "gets correct author" do
+  	ip = InProceeding.new title:"Otsikko", booktitle:"Kirjaotsikko", year:2014
+  	a = Author.new first_name:"Pekka", last_name:"Akkep"
+
+    ip.authors << a
+    expect(ip.authors.first).to eq(a)
+  end
 end
