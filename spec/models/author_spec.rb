a14f590642b8283f49e7bc9c44f63f3e4cc57c94
Julppu <juha.lindqvist@cs.helsinki.fi>
2014-04-04 07:59:35 +0300

    testiä

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -7,8 +7,8 @@ describe Author do
 	auth.last_name.should == "Meikalainen"
   end
 
-  it "has id set righr" do
-  	auth = Author.new first_name:"Matti", last_name:"Meikalainen"
+  it "has id set right" do
+  	auth = Author.new first_name:"Matti", last_name:"Meikalainen", id:1
   	auth.id.should == 1
   end
   
