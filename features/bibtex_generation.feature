73b744568b05da286f642c88d16741eb8d0e8c82
Sami Salonen <sami.salonen@cs.helsinki.fi>
2014-04-09 23:50:22 +0300

    why, oh why cucumber

---
+++ features/bibtex_generation.feature
@@ -0,0 +1,14 @@
+@wip
+Feature: Generation of bibtex-files
+
+  Scenario: Bibtex-generation works for Inproceedings-type references
+
+    When I generate the bibtex-file for said reference
+
+    #Given the following inproceedings reference exists:
+    #  |author|FDfdf|
+    #  |title|Fdsa|
+    #  |booktitle|DDDffff|
+    #  |year|1991|
+
+    #Then I navigate to '/inproceedings/1.bib'
--- features/step_definitions/all_steps.rb
+++ features/step_definitions/all_steps.rb
@@ -6,6 +6,18 @@ Given /^I navigate to (.*)$/ do |link|
   click_link link
 end
 
+Given /^the following inproceedings reference exists:$/ do |table|
+  new_inproceedings = Inproceedings.new
+  table.rows_hash.each do |field, value|
+    new_inproceedings[field] = value
+  end
+  new_inproceedings.save
+end
+
+When(/^I generate the bibtex\-file for said reference$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
 When /^I fill in (.*) as (.*)$/ do |stuff, field|
   fill_in(field, :with => stuff)
 end
