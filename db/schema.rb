5cc231fd86efc429374614a1581f56d4cf5e3105
Sami Salonen <sami.salonen@cs.helsinki.fi>
2014-04-10 01:11:45 +0300

    schema updated, deleted old inproceedings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409204015) do
+ActiveRecord::Schema.define(version: 20140409220716) do
 
   create_table "articles", force: true do |t|
     t.string   "author"
@@ -35,14 +35,6 @@ ActiveRecord::Schema.define(version: 20140409204015) do
     t.string   "title"
     t.string   "publisher"
     t.string   "year"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "in_proceedings", force: true do |t|
-    t.string   "title"
-    t.string   "booktitle"
-    t.integer  "year"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
